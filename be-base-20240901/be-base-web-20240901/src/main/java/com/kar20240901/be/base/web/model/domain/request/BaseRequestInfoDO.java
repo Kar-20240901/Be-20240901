a34c5fd60e4fd654d1b31a5286fd0620a1808e4f
Cmc0 <dimlt@vip.qq.com>
2024-10-24 18:46:44 +0800

    feat: 【i】【优化】【代码】

--- be-base-20240901/be-base-web-20240901/src/main/java/com/kar20240901/be/base/web/model/domain/request/BaseRequestInfoDO.java
+++ be-base-20240901/be-base-web-20240901/src/main/java/com/kar20240901/be/base/web/model/domain/request/BaseRequestInfoDO.java
@@ -2,10 +2,13 @@ package com.kar20240901.be.base.web.model.domain.request;
 
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
+@TableName(value = "base_request_info")
 @Data
+@Schema(description = "子表：请求详情表，主表：请求表")
 public class BaseRequestInfoDO {
 
     @TableId(type = IdType.INPUT)
