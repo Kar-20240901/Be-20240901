1b27b2e38971bd973d8de8bdbd737c4e4aefcfbd
Cmc0 <dimlt@vip.qq.com>
2025-04-17 14:30:17 +0800

    feat: 【i】【优化】【代码】

--- be-base-20240901/be-base-web-20240901/src/main/java/com/kar20240901/be/base/web/mapper/im/BaseImSessionContentMapper.java
+++
@@ -1,9 +0,0 @@
-package com.kar20240901.be.base.web.mapper.im;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface BaseImSessionContentMapper extends BaseMapper<BaseImSessionContentDO> {
-
-}
