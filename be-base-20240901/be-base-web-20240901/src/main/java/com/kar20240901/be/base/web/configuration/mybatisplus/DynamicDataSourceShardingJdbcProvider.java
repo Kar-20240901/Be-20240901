d68d44a853ea9fe9d856dd1d3a68c875f283ff69
Cmc0 <dimlt@vip.qq.com>
2025-08-13 14:43:49 +0800

    feat: 【i】【优化】【代码】

---
+++ be-base-20240901/be-base-web-20240901/src/main/java/com/kar20240901/be/base/web/configuration/mybatisplus/DynamicDataSourceShardingJdbcProvider.java
@@ -0,0 +1,78 @@
+package com.kar20240901.be.base.web.configuration.mybatisplus;
+
+import cn.hutool.core.map.MapUtil;
+import com.baomidou.dynamic.datasource.DynamicRoutingDataSource;
+import com.baomidou.dynamic.datasource.creator.DefaultDataSourceCreator;
+import com.baomidou.dynamic.datasource.provider.AbstractDataSourceProvider;
+import com.baomidou.dynamic.datasource.provider.DynamicDataSourceProvider;
+import com.baomidou.dynamic.datasource.spring.boot.autoconfigure.DynamicDataSourceAutoConfiguration;
+import com.baomidou.dynamic.datasource.spring.boot.autoconfigure.DynamicDataSourceProperties;
+import java.util.List;
+import java.util.Map;
+import javax.annotation.Resource;
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+/**
+ * 参考：{@link DynamicDataSourceAutoConfiguration}
+ */
+@Configuration
+public class DynamicDataSourceShardingJdbcProvider {
+
+    @Resource
+    DynamicDataSourceProperties dynamicDataSourceProperties;
+
+    @Resource
+    DefaultDataSourceCreator defaultDataSourceCreator;
+
+    /**
+     * 如果本类比 ShardingJdbc，加载更前面，就要加一个 @Lazy注解，防止注入不进来
+     */
+    @Qualifier("shardingSphereDataSource")
+    @Resource
+    DataSource shardingSphereDataSource;
+
+    @Bean
+    public DynamicDataSourceProvider dynamicDataSourceProvider() {
+
+        return new AbstractDataSourceProvider(defaultDataSourceCreator) {
+
+            @Override
+            public Map<String, DataSource> loadDataSources() {
+
+                Map<String, DataSource> dataSourceMap = MapUtil.newHashMap();
+
+                // 将 shardingJdbc 管理的数据源也交给动态数据源管理，并替换：默认数据源
+                dataSourceMap.put(dynamicDataSourceProperties.getPrimary(), shardingSphereDataSource);
+
+                return dataSourceMap;
+
+            }
+
+        };
+
+    }
+
+    /**
+     * Primary，注解的目的：因为 ShardingJdbc也会注册一个数据源，所以这里需要加 Primary注解 并且一定要在：DynamicDataSourceAutoConfiguration 之前加载该 Bean
+     */
+    @Primary
+    @Bean
+    public DataSource dataSource(List<DynamicDataSourceProvider> providers) {
+
+        DynamicRoutingDataSource dynamicRoutingDataSource = new DynamicRoutingDataSource(providers);
+
+        dynamicRoutingDataSource.setPrimary(dynamicDataSourceProperties.getPrimary());
+        dynamicRoutingDataSource.setStrict(dynamicDataSourceProperties.getStrict());
+        dynamicRoutingDataSource.setStrategy(dynamicDataSourceProperties.getStrategy());
+        dynamicRoutingDataSource.setP6spy(dynamicDataSourceProperties.getP6spy());
+        dynamicRoutingDataSource.setSeata(dynamicDataSourceProperties.getSeata());
+
+        return dynamicRoutingDataSource;
+
+    }
+
+}
