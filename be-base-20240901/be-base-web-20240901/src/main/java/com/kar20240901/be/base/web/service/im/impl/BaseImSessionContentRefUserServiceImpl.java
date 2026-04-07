4bbf76222038884429f1e9e79de1830ea0724da0
Cmc0 <dimlt@vip.qq.com>
2026-04-07 09:28:04 +0800

    feat: 【i】【优化】【代码】【hjzgwx】

--- be-base-20240901/be-base-web-20240901/src/main/java/com/kar20240901/be/base/web/service/im/impl/BaseImSessionContentRefUserServiceImpl.java
+++ be-base-20240901/be-base-web-20240901/src/main/java/com/kar20240901/be/base/web/service/im/impl/BaseImSessionContentRefUserServiceImpl.java
@@ -20,13 +20,24 @@ import com.kar20240901.be.base.web.service.im.BaseImSessionContentRefUserService
 import com.kar20240901.be.base.web.service.im.BaseImSessionRefUserService;
 import com.kar20240901.be.base.web.util.base.MyPageUtil;
 import com.kar20240901.be.base.web.util.base.MyUserUtil;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
 import javax.annotation.Resource;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class BaseImSessionContentRefUserServiceImpl
     extends ServiceImpl<BaseImSessionContentRefUserMapper, BaseImSessionContentRefUserDO>
     implements BaseImSessionContentRefUserService {
@@ -203,6 +214,148 @@ public class BaseImSessionContentRefUserServiceImpl
             .set(BaseImSessionContentRefUserDO::getShowFlag, false).update();
 
         return TempBizCodeEnum.OK;
+
+    }
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    static class Item {
+
+        private String name;
+
+        private String value;
+
+        private int type;
+
+        private String gap;
+
+    }
+
+    public static void main(String[] args) {
+
+        //        int base = 64 + 762; // 400
+        //
+        //        List<Item> list = new ArrayList<>();
+        //
+        //        list.add(new Item("伤害", "7", 102, "0.7")); // 700+240
+        //
+        //        list.add(new Item("智慧", "3.6", 201, "0.29")); // 720+
+        //        list.add(new Item("伤害额外", "0.5", 201, "0.15")); // 35.2+27.5+17+24.5+29+36.5+64+
+        //        list.add(new Item("法术额外", "0.5", 201, "0.15")); // 16.4+18.5+25
+        //        list.add(new Item("冰冷额外", "1", 201, "0.15")); // 41.5+13+166+64+112
+        //        list.add(new Item("投射物额外", "1.2", 201, "0.36")); // 40.5+135+
+        //        list.add(new Item("纠缠额外", "1.2", 201, "0.4")); // 40+40
+        //        list.add(new Item("冰封额外", "0.3", 201, "0.15")); // 10+10
+        //        list.add(new Item("软弱额外", "0.15", 201, "0.15")); // 15+
+        //        list.add(new Item("击中额外", "0.15", 201, "0.15")); // 35+
+        //        list.add(new Item("同时封印生命和魔力额外", "0.15", 201, "0.15")); // 10
+
+        int base = 64 + 762; // 826
+
+        List<Item> list = new ArrayList<>();
+
+        list.add(new Item("伤害", "9.4", 102, "0.7")); // 700+240
+
+        list.add(new Item("智慧", "3.6", 201, "0.29")); // 720+
+        list.add(new Item("伤害额外", "2.33", 201, "0.15")); // 35.2+27.5+17+24.5+29+36.5+64+
+        list.add(new Item("法术额外", "0.6", 201, "0.15")); // 16.4+18.5+25
+        list.add(new Item("冰冷额外", "3.96", 201, "0.15")); // 41.5+13+166+64+112
+        list.add(new Item("投射物额外", "1.35", 201, "0.3")); // 135+
+        list.add(new Item("纠缠额外", "0.8", 201, "0.4")); // 40+40
+        list.add(new Item("冰封额外", "0.2", 201, "0.1")); // 10+10
+        list.add(new Item("软弱额外", "0.15", 201, "0.15")); // 15+
+        list.add(new Item("击中额外", "0.35", 201, "0.15")); // 35+
+        list.add(new Item("同时封印生命和魔力额外", "0.1", 201, "0.1")); // 10
+
+        BigDecimal baseBigDecimal = calc(base, list, "原本", null, "60");
+
+        for (Item item : list) {
+
+            String originalValue = item.getValue();
+
+            item.setValue(new BigDecimal(originalValue).add(new BigDecimal(item.getGap())).toPlainString());
+
+            calc(base, list, "增加\033[35m" + item.getGap() + "\033[0m" + item.getName(), baseBigDecimal,
+                item.getGap());
+
+            item.setValue(originalValue);
+
+        }
+
+    }
+
+    /**
+     * 计算
+     */
+    private static BigDecimal calc(int base, List<Item> list, String remark, BigDecimal baseBigDecimal, String gap) {
+
+        BigDecimal result = new BigDecimal(base);
+
+        Map<Integer, List<Item>> map = new HashMap<>();
+
+        for (Item item : list) {
+
+            map.computeIfAbsent(item.getType(), k -> new ArrayList<>()).add(item);
+
+        }
+
+        for (Entry<Integer, List<Item>> entry : map.entrySet()) {
+
+            Integer key = entry.getKey();
+
+            List<Item> valueList = entry.getValue();
+
+            if (key >= 100 && key < 200) {
+
+                BigDecimal calc = new BigDecimal(1);
+
+                for (Item item : valueList) {
+
+                    BigDecimal bigDecimalTemp = new BigDecimal(item.getValue());
+
+                    calc = calc.add(bigDecimalTemp);
+
+                }
+
+                //                BigDecimal original = new BigDecimal(result.toPlainString());
+
+                result = result.multiply(calc);
+
+                //                log.info("inc，伤害：{}，倍率：{}", result.toPlainString(),
+                //                    result.divide(original, 2, RoundingMode.HALF_UP));
+
+            } else if (key >= 200 && key < 300) {
+
+                for (Item item : valueList) {
+
+                    BigDecimal bigDecimalTemp = new BigDecimal(item.getValue());
+
+                    //                    BigDecimal original = new BigDecimal(result.toPlainString());
+
+                    result = result.multiply(BigDecimal.valueOf(1).add(bigDecimalTemp));
+
+                    //                    log.info("more，{}，伤害：{}，倍率：{}", item.getName(), result.toPlainString(),
+                    //                        result.divide(original, 2, RoundingMode.HALF_UP));
+
+                }
+
+            }
+
+        }
+
+        if (baseBigDecimal == null) {
+
+            log.info("{}伤害：{}", remark, result.toPlainString());
+
+        } else {
+
+            log.info("{}，伤害：{}，最终增加：\033[36m{}\033[0m", remark, result.toPlainString(),
+                result.divide(baseBigDecimal, 2, RoundingMode.HALF_UP).subtract(BigDecimal.ONE));
+
+        }
+
+        return result;
 
     }
 
